a0c9a72dab479ccfe44cf70d0c5c241e73c4d4f3
Jiangfei <goujiangfei@blackpai.com>
2019-06-20 15:03:49 +0800

    依赖

--- speed/nodes/vct/main.go
+++ speed/nodes/vct/main.go
@@ -12,9 +12,10 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
-	// "github.com/Shopify/sarama"
+	"github.com/Shopify/sarama"
 	"github.com/buger/jsonparser"
 	"github.com/json-iterator/go"
 	"github.com/spf13/viper"
@@ -404,4 +405,29 @@ func loopReadAndPaser() {
 		// 	dbHeight++
 		// }
 	}
+}
+
+func kakfa() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll  //赋值为-1：这意味着producer在follower副本确认接收到数据后才算一次发送完成。
+	config.Producer.Partitioner = sarama.NewRandomPartitioner  //写到随机分区中，默认设置8个分区
+	config.Producer.Return.Successes = true
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = `nginx_log`
+	msg.Value = sarama.StringEncoder("this is a good test")
+	client,err := sarama.NewSyncProducer([]string{"127.0.0.1:9092"},config)
+	if err != nil{
+		fmt.Println("producer close err, ",err)
+		return
+	}
+	defer client.Close()
+	pid, offset, err := client.SendMessage(msg)
+ 
+	if err != nil{
+		fmt.Println("send message failed, ", err)
+		return
+	}
+	fmt.Printf("分区ID:%v, offset:%v \n", pid, offset)
+	}
+
 }
