44af23a68f8df4891bc67cad6f175763e9ffa118
Jiangfei <goujiangfei@blackpai.com>
2019-07-16 20:17:41 +0800

    确认库

--- builder/nodes/vct/main.go
+++ builder/nodes/vct/main.go
@@ -272,11 +272,12 @@ func setSendTransactionTxid(requestID, txid string) {
 func setSendTransactionError(requestID, txid string) {
 	// const transfer = await this.commdb.models.Transfer.findById(mid)
 	commDb := "dynasty"
+	chaindb := "vct"
 	where := bson.M{"_id": requestID}
 	ctx := context.Background()
-	result := db.GetCollection(commDb, "transfers").FindOneAndDelete(ctx, where)
+	result := db.GetCollection(chaindb, "transferstochain").FindOneAndDelete(ctx, bson.M{"requestID": requestID})
 	if result.Err() != nil {
-		fmt.Errorf("%s \n", "transfer not found!")
+		fmt.Println("transfer not found!")
 	}
 	// if !transfer {
 	// 	fmt.Errorf("%s \n", "transfer not found!")
@@ -284,7 +285,7 @@ func setSendTransactionError(requestID, txid string) {
 	// }
 	// 更新转账操作记录
 	updateStr := bson.M{"$set": bson.M{"txid": txid, "code": 16, "status": `TXID`, "updatedAt": time.Now().Unix()}, "$push": bson.M{"logs": `TX_HASH at: ${Date.now()}`}}
-	db.GetCollection(commDb, "transfers").FindOneAndUpdate(ctx, requestID, updateStr)
+	db.GetCollection(commDb, "transfers").FindOneAndUpdate(ctx, where, updateStr)
 	// // 更新转账账单记录
 	// const updateStr1 = { $set: { code: 16, txid, updatedAt: Date.now() } }
 	// const tc = await this.chaindb.models.TransferToChain.findOneAndUpdate({ requestId: mid }, updateStr1)
@@ -324,4 +325,109 @@ func newTranferFromChain(tfc models.TransferFromChain, haveComfirming bool) {
 		db.GetCollection(dbname, "transferfromchains").FindOneAndUpdate(context.Background(), bson.M{"txid": tx.Txid}, updateStr, op)
 	}
 
+}
+
+func newBlockNotify(blockNumber string) {
+	// if (this.inProcess) return
+
+	// this.inProcess = true
+	chaindb := "vct"
+	commdb := "dynasty"
+	ctx := context.Background()
+	// const safeBlockNumber = blockNumber - this.confirmedMaxNum + 1
+	// // this.logger.info('[DynastyThreadUtil:newBlockNotify]', blockNumber, 'safeBlockNumber', safeBlockNumber)
+	// const tcs = await this.chaindb.models.TransferConfirming.find().limit(10240)
+	op := options.Find().SetLimit(1024)
+	result, err := db.GetCollection(chaindb, "transferconfirmings").Find(ctx, bson.M{}, op)
+	if err != nil {
+		fmt.Println("transfer not found!")
+	}
+	if result.Next() {
+
+	}
+
+	// 	const ts = Date.now()
+
+	// 	for (const tc of tcs) {
+	// 		try {
+	// 			const { blockHeight, requestId, from, to } = tc
+	// 			// 确认完毕
+	// 			const isFinish = blockHeight <= safeBlockNumber
+	// 			const confirmedNum = blockNumber - blockHeight + 1
+
+	// 			// console.log(tc.toObject(), isFinish, blockHeight, safeBlockNumber)
+
+	// 			if (isFinish) {
+	// 				// 1. 创建TransferFromChain
+	// 				let tfc
+	// 				try {
+	// 					// "$isolated": true
+	// 					tfc = await this.chaindb.models.TransferFromChain.findByIdAndUpdate(tc._id, { $set: tc.toObject() }, { upsert: true, new: true })
+	// 				} catch (e) {
+	// 					if (e.message.indexOf('E11000') >= 0) {
+	// 						this.logger.warn(e.message)
+	// 					}
+	// 				}
+	// 				// 2. 修改转账记录状态
+	// 				if (requestId) {
+	// 					await this.commdb.models.Transfer.findByIdAndUpdate(requestId, { $set: { code: 64, status: 'FINISH', updatedAt: ts }, $push: { logs: `confirm finish at ${ts}` } })
+	// 				}
+	// 				await this.chaindb.models.TransferConfirming.findByIdAndDelete(tc._id)
+	// 				const froms = Array.isArray(from) ? from : [from]
+	// 				for (const addr of froms) {
+	// 					const fromSubscribed = this.addressMap[addr]
+	// 					if (fromSubscribed) {
+	// 						await this._finish(fromSubscribed, addr, tfc, 'OUT')
+	// 					}
+	// 				}
+	// 				const tos = Array.isArray(to) ? to : [to]
+	// 				for (const addr of tos) {
+	// 					const toSubscribed = this.addressMap[addr]
+	// 					if (toSubscribed) {
+	// 						console.log('__toall finish(IN):', addr)
+	// 						await this._finish(toSubscribed, addr, tfc, 'IN')
+	// 					}
+	// 				}
+
+	// 				// 一笔交易, 钱包里还会包含一笔到from的找零
+	// 				if ((this.chainSymbol === 'BTC' || this.chainSymbol === 'LTC') && tc.outs && tc.outs.length > 0) {
+	// 					for (const out of tc.outs) {
+	// 						const o = tos.find(item => String(item) === String(out.address))
+	// 						if (!o || o.length === 0) {
+	// 							const toSubscribed = this.addressMap[out.address]
+	// 							if (toSubscribed) {
+	// 								console.log('__toall finish(IN)(From):', out.address)
+	// 								await this._finish(toSubscribed, out.address, tfc, 'IN')
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+
+	// 				// 1. 删除待确认记录
+	// 				await this.chaindb.models.TransferConfirming.findByIdAndDelete(tc._id)
+	// 			} else {
+	// 				const froms = Array.isArray(from) ? from : [from]
+	// 				for (const addr of froms) {
+	// 					const fromSubscribed = this.addressMap[addr]
+	// 					if (fromSubscribed) {
+	// 						this.logger.info('__toall _confirm(OUT):', confirmedNum, blockNumber)
+	// 						await this._confirm(fromSubscribed, tc._id, 'OUT', confirmedNum, blockNumber)
+	// 					}
+	// 				}
+	// 				const tos = Array.isArray(to) ? to : [to]
+	// 				for (const addr of tos) {
+	// 					const toSubscribed = this.addressMap[addr]
+	// 					if (toSubscribed) {
+	// 						this.logger.info('__toall _confirm(IN):', confirmedNum, blockNumber)
+	// 						await this._confirm(toSubscribed, tc._id, 'IN', confirmedNum, blockNumber)
+	// 					}
+	// 				}
+	// 			}
+	// 		} catch (e) {
+	// 			this.logger.error('[DynastyThreadUtil:newBlockNotify]', e)
+	// 		}
+	// 	}
+	// } finally {
+	// 	// this.inProcess = false
+	// }
 }
