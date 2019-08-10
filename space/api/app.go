90296abb16713725f15a1a18cc943dd101ccd105
Jiangfei <goujiangfei@blackpai.com>
2019-08-10 19:55:24 +0800

    space 转发

--- space/api/app.go
+++ space/api/app.go
@@ -1,13 +1,66 @@
 package api
 
-import "net/http"
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type App struct {
+	baseUrl string
+}
+type AppInterface interface {
+	RedirectGet(path, query string) ([]byte, error)
+	RedirectPsot(path, requestBody string) ([]byte, error)
+	RedirectAny(ctx *gin.Context) ([]byte, error)
+}
+
+func newApp(base string) AppInterface {
+	return &App{
+		baseUrl: base,
+	}
+}
 
 //RedirectGet RedirectGet
-func RedirectGet(path string, query []byte) *http.Response {
-
-	resp, err := http.Get(path)
+func (a *App) RedirectGet(path, query string) ([]byte, error) {
+	body := path + "?" + query
+	resp, err := http.Get(body)
 	if err != nil {
-
+		return nil, err
 	}
-	return resp
+
+	respbody, err := ioutil.ReadAll(resp.Body)
+	return respbody, err
+	// return resp
+}
+
+func (a *App) RedirectPsot(path, requestBody string) ([]byte, error) {
+	body := strings.NewReader(requestBody)
+	resp, err := http.Post(path, "application/x-www-form-urlencoded", body)
+	if err != nil {
+		return nil, err
+	}
+
+	respbody, err := ioutil.ReadAll(resp.Body)
+	return respbody, err
+	// return resp
+}
+
+func (a *App) RedirectAny(ctx *gin.Context) ([]byte, error) {
+	url := a.baseUrl + ctx.Request.URL.Path
+	if ctx.Request.Method == "GET" {
+		query := ctx.Request.URL.RawQuery
+		return a.RedirectGet(url, query)
+	}
+
+	resp, err := http.Post(url, "application/x-www-form-urlencoded", ctx.Request.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	respbody, err := ioutil.ReadAll(resp.Body)
+	return respbody, err
+	// return resp
 }
--- space/main.go
+++ space/main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"century/oasis/space/api"
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
-var db map[string]string
+var (
+	envprefix  = "space"
+	configName = "config"
+	env        = "dev"
+	db         map[string]string
+	app        api.AppInterface
+)
 
 func balance(c *gin.Context) {
-	// c.String(http.StatusOK, "pong")
-	api.RedirectGet(c.Request.URL.Path, c.GetRawData)
+	baseURL := viper.GetString(env + ".baseUrl")
+	url := baseURL + c.Request.URL.Path
+	query := c.Request.URL.RawQuery
+	resp, err := api.RedirectGet(url, query)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"code": 40000, "msg": err.Error()})
+		return
+	}
+
+	c.Writer.Write(resp)
+
+	// c.JSON(http.StatusOK, resp)
+}
+func any(c *gin.Context) {
+	resp, err := app.RedirectAny(c)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"code": 40000, "msg": err.Error()})
+		return
+	}
+
+	c.Writer.Write(resp)
 }
 
 func setupRouter() *gin.Engine {
@@ -23,7 +54,6 @@ func setupRouter() *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-	// r.Group("/api").Group("/v1").GET("/balance", balance)
 
 	// Get user value
 	r.GET("/user/:name", func(c *gin.Context) {
@@ -66,6 +96,8 @@ func setupRouter() *gin.Engine {
 	{
 		index.GET("/balance", balance)
 	}
+
+	r.Any("*", any)
 	// r.GET("/balance", &controllers.BalanceController{}),
 	// r.GET("/createTransferTxData", ft, "post:CreateTransferTxData"),
 	// r.GET("/submitTx", ft, "post:SubmitTx"),
@@ -78,8 +110,55 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
+func initConf() {
+
+	gopath := os.Getenv("GOPATH")
+	path := []string{}
+
+	for _, p := range filepath.SplitList(gopath) {
+		// century\oasis\space
+		pathConf := filepath.Join(p, "src/century/oasis/space")
+		path = append(path, pathConf)
+		// viper.AddConfigPath(path)
+	}
+	path = append(path, ".")
+	e := InitViper(envprefix, configName, path)
+	if e != nil {
+		fmt.Println(e)
+	}
+}
+
+//InitViper we can set viper which fabric peer is used
+func InitViper(envprefix string, filename string, configPath []string) error {
+	fmt.Println("envprefix", envprefix, "filename", filename, "configPath", configPath)
+	viper.SetEnvPrefix(envprefix)
+	viper.AutomaticEnv()
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+
+	for _, c := range configPath {
+		viper.AddConfigPath(c)
+	}
+
+	viper.SetConfigName(filename) // Name of config file (without extension)
+	return viper.ReadInConfig()   // Find and read the config file
+
+}
+
 func main() {
+	flag.StringVar(&env, "env", "dev", "env")
+	flag.Parse()
+	initConf()
+
+	baseURL := viper.GetString(env + ".baseUrl")
+	app = newApp(baseURL)
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":7789")
+	host := viper.GetInt(env + ".host")
+	if host == 0 {
+		host = 7788
+	}
+	listen := fmt.Sprintf(":%d", host)
+
+	r.Run(listen)
 }
